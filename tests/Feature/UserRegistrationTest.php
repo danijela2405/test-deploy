990edd501f48315199754cbfe031e7b9c0fa931f
danijela2405 <danijela2405@gmail.com>
2025-06-25 16:30:51 +0200

    add tests

---
+++ tests/Feature/UserRegistrationTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserRegistrationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     */
+    public function test_guest_can_register(): void
+    {
+        $response = $this->post('/register',
+    [
+        'name'=>'neko ime',
+        'email'=>'email123@email.com',
+        'password'=>'password',
+        ]);
+
+        $user = User::where('email', 'email123@email.com')->first();
+
+        $this->assertNotNull( $user);
+        $this->assertSame('neko ime', $user->name);
+        $response->assertRedirect('/users');
+
+    }
+}
